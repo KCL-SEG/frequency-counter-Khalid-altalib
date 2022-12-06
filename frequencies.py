a95a9482eb24dcf6c20455fd0f05e5e9dab9067d
Khalid-altalib <66210571+Khalid-altalib@users.noreply.github.com>
2022-12-06 18:40:15 +0000

    Full solution

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,13 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+from collections import Counter
 def frequencies(items):
-    frequencies = {}
-    # Your code goes here
+
+    strings = [str(x) for x in items] # convert all items to strings
+    frequencies = Counter(strings) # count the frequencies of each string
+    #print(frequencies)
+
     return frequencies
+
+#frequencies(['0', 4,4,'4','d','d','e',0,'a','d','4'])
